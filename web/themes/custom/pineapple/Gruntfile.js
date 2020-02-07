4dbbe2b5599ec23977923e0981053d4ea106966a
katarinalazarova <katarinaalazarova@gmail.com>
2020-02-07 19:29:48 +0000

    removing the __field.scss file and corresponding references in the code

--- web/themes/custom/pineapple/Gruntfile.js
+++ web/themes/custom/pineapple/Gruntfile.js
@@ -29,8 +29,7 @@ module.exports = function(grunt) {
           'sass/core/__core.scss': 'sass/core/**/*.scss',
           'sass/base/__base.scss': 'sass/base/**/*.scss',
           'sass/layouts/__layouts.scss': 'sass/layouts/**/*.scss',
-          'sass/components/__components.scss': 'sass/components/**/*.scss',
-          'sass/components/field/__field.scss': 'sass/field/**/*.scss'
+          'sass/components/__components.scss': 'sass/components/**/*.scss'
         },
         options: {
           useSingleQuotes: false,
