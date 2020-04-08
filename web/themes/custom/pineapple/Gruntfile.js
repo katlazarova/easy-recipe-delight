62d04ec4106276951b1bbb31e9ca23978ceec0ae
katarinalazarova <katarinaalazarova@gmail.com>
2020-04-08 16:14:49 +0100

    changing the comment body field to be formatted to use restricted html format, minor styling changes

--- web/themes/custom/pineapple/Gruntfile.js
+++ web/themes/custom/pineapple/Gruntfile.js
@@ -30,7 +30,6 @@ module.exports = function(grunt) {
           'sass/base/__base.scss': 'sass/base/**/*.scss',
           'sass/layouts/__layouts.scss': 'sass/layouts/**/*.scss',
           'sass/components/__components.scss': 'sass/components/**/*.scss',
-          'sass/content/__content.scss': 'sass/content/**/*.scss'
         },
         options: {
           useSingleQuotes: false,
