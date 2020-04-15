8eee25cd744061093973c3e5583fa8151353b006
katarinalazarova <katarinaalazarova@gmail.com>
2020-04-15 15:04:18 +0100

    adding accordion behavior js

---
+++ web/themes/custom/pineapple/assets/js/accordion.behavior.js
@@ -0,0 +1,16 @@
+(function ($, Drupal) {
+
+  Drupal.behaviors.accordion = {
+    attach: function (context, settings) {
+      $('.accordion-item .accordion-title').once('click-once').each(function () {
+        $(this).click(function () {
+          // Toggle the panel content on click.
+          $(this).next('.accordion-content').slideToggle();
+          // With each click on the accordion, toggle the active class.
+          console.log(this);
+          $(this).toggleClass('is-open');
+        });
+      });
+    },
+  };
+}(jQuery, Drupal));
