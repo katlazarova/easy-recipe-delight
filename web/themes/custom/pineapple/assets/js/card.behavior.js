9e63584ae5cbc8be7316d24c2a90b7cff6846efd
katarinalazarova <katarinaalazarova@gmail.com>
2020-04-27 15:22:28 +0100

    bugfix - updating card js

--- web/themes/custom/pineapple/assets/js/card.behavior.js
+++ web/themes/custom/pineapple/assets/js/card.behavior.js
@@ -1,33 +1,30 @@
-(function ($, Drupal) {
-
-  Drupal.behaviors.card = {
-    attach: function (context, settings) {
-      $('[data-card]', context).once('card')
-      .each(function eachCard() {
-        const $link = $(this).find('a').first();
-
+(function card($, Drupal) {
+  Drupal.behaviors.ClickableCard = {
+    attach: function attach(context) {
+      $('[data-card]', context).once('clickable-card').each(function eachCard() {
+        // https://inclusive-components.design/cards/
+        var $link = $(this).find('a').first();
         if ($link.length === 0 || $(this).data().cardClickable === false) {
           return;
         }
-
         $(this).css('cursor', 'pointer');
-
-        let down;
-        let up;
-
-        $(this).mousedown(() => {
+        var down;
+        var up;
+        $(this).mousedown(function () {
           down = +new Date();
         });
-
-        $(this).mouseup(e => {
+        $(this).mouseup(function (e) {
+          // Bail if this was a middle or secondary mouse button event.
           if (e.which !== 1) return;
-
           up = +new Date();
-          if (up - down < 600) {
+          if (up - down < 200) {
+            // Use the time difference between mouse down and mouse up to
+            // guess whether the user is clicking, or trying to select text.
+            // Only redirect if it looks like a click not text selection.
             window.location.href = $link.attr('href');
           }
         });
       });
-    },
+    }
   };
-}(jQuery, Drupal));
+})(jQuery, Drupal);
